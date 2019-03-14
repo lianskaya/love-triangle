acfb4ab2179f9f84004218ff0de4b9a773c9e560
Marina Lianskaya <marinalianskaya@gmail.com>
2019-03-14 16:21:31 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+ 
+ module.exports = function getLoveTrianglesCount(preferences = []) {
+   var number = 0;
+   for (count = 0;count<preferences.length; count++){
+   if(preferences[preferences[preferences[count]-1]-1]-1 == count )
+    number++;
+   }
+   return parseInt(number/3);
+
 };
